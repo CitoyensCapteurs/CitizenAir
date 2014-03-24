e241702eb7247aaf4ad10cef4f614c5c7e485552
Phyks <webmaster@phyks.me>
2014-03-24 14:43:46 +0100

    Mise à jour de la licence dans chaque fichier

--- api.php
+++ api.php
@@ -1,4 +1,20 @@
 <?php
+/* 
+ * This file is part of CitizenAir.
+ * CitizenAir is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CitizenAir is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with CitizenAir.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 if(!is_file('data/types.data') || !is_file('api.keys')) {
     header('HTTP/1.1 400 Bad Request');
     exit();
